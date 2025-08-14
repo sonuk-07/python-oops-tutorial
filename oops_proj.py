57f53175c1b4b83c40d3d8228e617998c0154115
sonu <skjais04@gmail.com>
2025-08-14 17:55:33 +0545

    upload post and send message method created

--- oops_proj.py
+++ oops_proj.py
@@ -19,9 +19,9 @@ class chatbook:
         elif user_input == '2':
             self.signup()
         elif user_input == '3':
-            pass
+            self.upload_post()
         elif user_input == '4':
-            pass
+            self.send_message()
         elif user_input == '5':
             print("Exiting chatbook. Goodbye!")
             exit()
@@ -49,6 +49,25 @@ class chatbook:
                 print("Invalid credentials, please try again.")
         else:
             print("No user registered. Please register first.")
+        self.menu()
+
+
+
+    def upload_post(self):
+        if self.logged_in:
+            post_content = input("Enter your post content: ")
+            print(f"Post uploaded: {post_content}")
+        else:
+            print("You need to log in first.")
+        self.menu()
+
+    def send_message(self):
+        if self.logged_in:
+            friend = input("Enter the name of your friend: ")
+            message = input("Enter your message: ")
+            print(f"Message sent to {friend}: {message}")
+        else:
+            print("You need to log in first.")
         self.menu()
 
 
